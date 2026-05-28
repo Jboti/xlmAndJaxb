1349940b16021b551a5d83d9a3633b2297093aeb
Jboti <jboti95@gmail.com>
2026-05-28 14:19:52 +0200

    customers.xsd for reverse pipeline

--- src/main/java/org/example/config/ConfigLoader.java
+++ src/main/java/org/example/config/ConfigLoader.java
@@ -3,7 +3,6 @@ package org.example.config;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
-
 public class ConfigLoader {
 
     private static final Properties props = new Properties();
